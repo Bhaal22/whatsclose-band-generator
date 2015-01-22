404374f96fc0871eb2018c3c06a7b5eefe051d5b
Bhaal22 <muller.john@gmail.com>
2015-01-22 23:10:54 +0100

    first template for band module

--- src/generator-whatsclose-band/app/index.js
+++ src/generator-whatsclose-band/app/index.js
@@ -5,14 +5,18 @@ module.exports = generators.Base.extend({
     // Calling the super constructor is important so our generator is correctly set up
     generators.Base.apply(this, arguments);
 
-    // Next, add your custom code
-    //this.option('coffee'); // This method adds support for a `--coffee` flag
+    this.argument('bandName', { type: String, required: true });
   },
 
-  method1: function () {
-    console.log('method 1 just ran');
-  },
-  method2: function () {
-    console.log('method 2 just ran');
+  prompting: function () {
+    var done = this.async();
+    this.prompt({
+      type    : 'input',
+      name    : 'website',
+      message : 'Band WebSite'
+    }, function (answers) {
+      this.log(answers.name);
+      done();
+    }.bind(this));
   }
 });
