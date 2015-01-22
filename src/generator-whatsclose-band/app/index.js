f930c2a4e85ef959c0ff299618ecffd89b3b87dd
Bhaal22 <muller.john@gmail.com>
2015-01-22 23:22:59 +0100

    removed temp emacs file

--- src/generator-whatsclose-band/app/index.js
+++ src/generator-whatsclose-band/app/index.js
@@ -6,6 +6,7 @@ module.exports = generators.Base.extend({
     generators.Base.apply(this, arguments);
 
     this.argument('bandName', { type: String, required: true });
+    this.bandName = this._.humanize(this.bandName);
   },
 
   prompting: function () {
@@ -16,6 +17,7 @@ module.exports = generators.Base.extend({
       message : 'Band WebSite'
     }, function (answers) {
       this.log(answers.name);
+      this.log(this.bandName);
       done();
     }.bind(this));
   }
